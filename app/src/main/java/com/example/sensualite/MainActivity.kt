d2546ee58faee9d965340139852cc580ad403945
LuisZL477 <luis21985860@gmail.com>
2024-03-28 22:11:37 -0600

    Segunda Actualización

--- app/src/main/java/com/example/sensualite/MainActivity.kt
+++ app/src/main/java/com/example/sensualite/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.sensualite
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.registro)
+
+        button.setOnClickListener {
+            val intent=Intent(this,Registro::class.java)
+            startActivity(intent)
+        }
+
 
         Thread.sleep(1000)
 
--- app/src/main/java/com/example/sensualite/Registro.kt
+++ app/src/main/java/com/example/sensualite/Registro.kt
@@ -3,9 +3,25 @@ package com.example.sensualite
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.ArrayAdapter
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.sensualite.databinding.ActivityRegistroBinding
+
 class Registro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistroBinding
+
+    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val items = listOf("19","20","21","22","23","24", "25", "26", "27", "29", "30", "31" , "32")
+        val adapter = ArrayAdapter(this, R.layout.list_item, items)
+        binding.edad.setAdapter(adapter)
+
+
     }
 }
