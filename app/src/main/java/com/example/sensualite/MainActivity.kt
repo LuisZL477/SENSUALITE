04bcd6e0bb934c30a2f3db4e070b04b5d45965f2
LuisZL477 <luis21985860@gmail.com>
2024-03-28 18:43:55 -0600

    Primera Actualización

--- app/src/main/java/com/example/sensualite/MainActivity.kt
+++ app/src/main/java/com/example/sensualite/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.sensualite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Thread.sleep(1000)
+
     }
 }
