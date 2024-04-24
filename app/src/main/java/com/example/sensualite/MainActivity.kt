232bf8da4047e52eee812ed8c40c17fd875c454b
LuisZL477 <150941404+LuisZL477@users.noreply.github.com>
2024-04-23 22:00:55 -0600

    Actualización 6

--- app/src/main/java/com/example/sensualite/MainActivity.kt
+++ app/src/main/java/com/example/sensualite/MainActivity.kt
@@ -5,33 +5,74 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.sensualite.databinding.ActivityMainBinding
+import java.sql.SQLException
 import kotlin.concurrent.thread
+lateinit var edittextemail: EditText
+lateinit var edittextcontraseña: EditText
+lateinit var btn_inicio_sesion: Button
 
+// MainActivity.kt
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var connectSql = ConnectSql()
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        val button = findViewById<Button>(R.id.registro)
+        setContentView(binding.root)
 
-        button.setOnClickListener {
-            val intent=Intent(this,Registro::class.java)
+        edittextemail= findViewById(R.id.email)
+        edittextcontraseña = findViewById(R.id.contra)
+        btn_inicio_sesion = findViewById(R.id.iniciar_sesión)
+
+        val buttonRegistro = findViewById<Button>(R.id.registro)
+
+        buttonRegistro.setOnClickListener {
+            val intent = Intent(this, Registro::class.java)
             startActivity(intent)
         }
 
-       val button1 = findViewById<Button>(R.id.iniciar_sesión)
+        btn_inicio_sesion.setOnClickListener {
+            val email = edittextemail.text.toString()
+            val contraseña = edittextcontraseña.text.toString()
 
-        button1.setOnClickListener {
-            val intent = Intent(this,Inicio::class.java)
-            startActivity(intent)
+            if(email.isEmpty() || contraseña.isEmpty()){
+                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
+            }else{
+                try{
+                    val conn = connectSql.dbConn()
+                    val query = "SELECT * FROM Usuarios WHERE email = ? AND contraseña = ?"
+                    val stmt = conn?.prepareStatement(query)
+                    stmt?.setString(1, email)
+                    stmt?.setString(2, contraseña)
+                    val resultSet = stmt?.executeQuery()
+
+                    if(resultSet?.next() == true){
+                        Toast.makeText(this,"Inicio de sesión exitoso", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, Inicio::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, "Correo eléctrónico o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+                    }
+                }catch (ex: SQLException){
+                    Toast.makeText(this,"Error", Toast.LENGTH_SHORT).show()
+                }
+
+
+            }
+
+
+
         }
-
 
         Thread.sleep(1000)
-
     }
 }
--- app/src/main/java/com/example/sensualite/Registro.kt
+++ app/src/main/java/com/example/sensualite/Registro.kt
@@ -5,15 +5,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.sensualite.databinding.ActivityRegistroBinding
+import java.sql.PreparedStatement
+import java.sql.SQLException
+import android.text.InputFilter
+import android.text.Spanned
+
+
+lateinit var editText1: EditText
+lateinit var editText2: EditText
+lateinit var editText3: EditText
+lateinit var edad:AutoCompleteTextView
+lateinit var editText4: EditText
+lateinit var editText5: EditText
+lateinit var editText6: EditText
+lateinit var editText7: EditText
+lateinit var btn_registro: Button
 
 class Registro : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegistroBinding
 
-    
+    private var connectSql = ConnectSql()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +49,15 @@ class Registro : AppCompatActivity() {
             ,"89","90","91","92","93","94","95","96","97","98"
             ,"99","100")
 
+        editText1 =findViewById(R.id.nombre)
+        editText2 = findViewById(R.id.apaterno)
+        editText3 = findViewById(R.id.amaterno)
+        edad = findViewById(R.id.edad)
+        editText4 = findViewById(R.id.cpostal)
+        editText5 = findViewById(R.id.correo)
+        editText6 = findViewById(R.id.contraseña)
+        editText7 = findViewById(R.id.confirmacion_contraseña)
+        btn_registro = findViewById(R.id.registro)
 
         val adapter = ArrayAdapter(this, R.layout.list_item, items)
         binding.edad.setAdapter(adapter)
@@ -42,7 +70,51 @@ class Registro : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btn_registro.setOnClickListener {
+            val nombre = editText1.text.toString()
+            val apaterno = editText2.text.toString()
+            val amaterno = editText3.text.toString()
+            val edadSeleccionada = edad.text.toString()
+            val codigoPostalString = editText4.text.toString()
+            val correo = editText5.text.toString()
+            val contraseña = editText6.text.toString()
+            val confirmacionContraseña = editText7.text.toString()
 
+            if (nombre.isEmpty() || apaterno.isEmpty() || amaterno.isEmpty() || edadSeleccionada.isEmpty() || codigoPostalString.isEmpty() || correo.isEmpty() || contraseña.isEmpty() || confirmacionContraseña.isEmpty()) {
+                Toast.makeText(this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show()
+            } else if (contraseña != confirmacionContraseña) {
+                Toast.makeText(this, "La confirmación de contraseña no coincide", Toast.LENGTH_SHORT).show()
+            } else {
+                try {
+                    val conn = connectSql.dbConn()
+                    val query = "SELECT COUNT(*) FROM Usuarios WHERE email = ?"
+                    val stmt = conn?.prepareStatement(query)
+                    stmt?.setString(1, correo)
+                    val resultSet = stmt?.executeQuery()
+                    resultSet?.next()
+                    val count = resultSet?.getInt(1) ?: 0
+                    if (count > 0) {
+                        Toast.makeText(this, "El correo electrónico ya está registrado", Toast.LENGTH_SHORT).show()
+                    } else {
+                        val addUsuario: PreparedStatement =
+                            connectSql.dbConn()?.prepareStatement("INSERT INTO Usuarios VALUES (?,?,?,?,?,?,?)")!!
+                        addUsuario.setString(1, nombre)
+                        addUsuario.setString(2, apaterno)
+                        addUsuario.setString(3, amaterno)
+                        addUsuario.setString(4, edadSeleccionada)
+                        val codigoPostalInt = codigoPostalString.toInt()
+                        addUsuario.setInt(5, codigoPostalInt)
+                        addUsuario.setString(6, correo)
+                        addUsuario.setString(7, contraseña)
 
+                        addUsuario.executeUpdate()
+
+                        Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (ex: SQLException) {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
